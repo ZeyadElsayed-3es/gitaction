1368f69a30b34dfc7cb9bb52d61ce46db79d529f
ZeyadElsayed-3es <zeyad.elsayed@3es-eg.com>
2024-09-08 12:10:13 +0300

    Create refresh_token.sh

---
+++ refresh_token.sh
@@ -0,0 +1,6 @@
+curl --location -g --request POST 'https://euapi.ttlock.com/oauth2/token' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'clientId=b0dda4a4a3c74e6c9098df604b9a0288' \
+--data-urlencode 'clientSecret=36ced98de1c6a0a56f985b5ab2f3703f' \
+--data-urlencode 'grant_type=refresh_token' \
+--data-urlencode 'refresh_token=c16dc9cf6d8d473814de717d8ee6433d'   
