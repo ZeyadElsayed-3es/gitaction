05a4c4c368d15ee77eadce7f713fbe7f7610a792
ZeyadElsayed-3es <zeyad.elsayed@3es-eg.com>
2024-09-15 15:31:05 +0300

    Update renewal.py

--- renewal.py
+++ renewal.py
@@ -3,7 +3,7 @@ import subprocess
 #from git import Repo
 #---- Global Variables ------
 #CF_Token = os.environ['CF_Token']
-domain = os.getenv("domain")
+#domain = os.getenv("domain")
 def renew_cert(domain) :
     domain = "office.3es-eg.uk"
     Repo.clone_from('https://github.com/3Es-for-smart-solutions/scripts', 'scripts')
