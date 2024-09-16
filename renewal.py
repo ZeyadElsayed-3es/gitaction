4f21886757ec4d3f9e6cbcf5f9456084dfe08942
ZeyadElsayed-3es <zeyad.elsayed@3es-eg.com>
2024-09-16 12:57:46 +0300

    Update renewal.py

--- renewal.py
+++ renewal.py
@@ -4,26 +4,26 @@ from git import Repo
 #---- Global Variables ------
 CF_Token = os.environ['CF_Token']
 domain = os.environ['domain']
-def renew_cert() :
-    domain = "cloud.3es-eg.uk"
+def renew_cert(domain) :
+    domain = domain
     Repo.clone_from('https://github.com/Neilpang/acme.sh.git', 'acme.sh')
     os.chdir("acme.sh")
-    #command = "./acme.sh --issue --dns dns_cf --ocsp-must-staple --server letsencrypt  --keylength ec-384 -d %s --dnssleep 120 --days 90 --home '%s'" %(domain,domain)
-    #result = subprocess.run(command,shell=True)
-    #if (result.returncode == 0 ):
-        #print("Certificate renewed successfully.")
-        #cert_path = str("./"+domain + "_ecc/" + domain + ".cer")
-    result2 = subprocess.run(["scp","-P 2222","./README.md","zeyad@154.180.31.255:/home/zeyad/Desktop/" ])
+    command = "./acme.sh --issue --dns dns_cf --ocsp-must-staple --server letsencrypt  --keylength ec-384 -d %s --dnssleep 120 --days 90 --home '%s'" %(domain,domain)
+    result = subprocess.run(command,shell=True)
+    if (result.returncode == 0 ):
+        print("Certificate renewed successfully.")
+        cert_path = str("./"+domain + "_ecc/" + domain + ".cer")
+    result2 = subprocess.run(["scp","-P 2222", cert_path ,"zeyad@154.180.31.255:/home/zeyad/Desktop/" ])
         
 #------------the output ----------------
-        #if (result2.returncode == 0 ):
-         #   print("Certificate renewed and transfared successfully.")
-        #else:
-         #   print("Failed to transfer certificate.")
-          #  print(result.stderr)
-    #else:
-     #   print("Failed to renew certificate.")
-      #  print(result.stderr)
+        if (result2.returncode == 0 ):
+            print("Certificate renewed and transfared successfully.")
+        else:
+           print("Failed to transfer certificate.")
+           print(result.stderr)
+    else:
+        print("Failed to renew certificate.")
+        print(result.stderr)
 
 
-renew_cert()
+renew_cert(domain)
