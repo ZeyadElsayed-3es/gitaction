d506ab56cb045ff05a17f7ad2ed1238b5fee4d19
ZeyadElsayed-3es <zeyad.elsayed@3es-eg.com>
2024-09-16 10:34:21 +0300

    Update renewal.py

--- renewal.py
+++ renewal.py
@@ -8,12 +8,12 @@ def renew_cert() :
     domain = "cloud.3es-eg.uk"
     Repo.clone_from('https://github.com/Neilpang/acme.sh.git', 'acme-cert-bot')
     os.chdir("acme-cert-bot")
-    command = "./acme.sh --issue --dns dns_cf --ocsp-must-staple --server letsencrypt  --keylength ec-384 -d %s --dnssleep 120 --days 90 --home '%s'" %(domain,domain)
-    result = subprocess.run(command,shell=True)
-    if (result.returncode == 0 ):
-        print("Certificate renewed successfully.")
-        cert_path = str("./"+domain + "_ecc/" + domain + ".cer")
-        result2 = subprocess.run(["scp -P 2222 ",cert_path,"zeyad@office.3es-eg.uk:/home/zeyad/Desktop/" ])
+    #command = "./acme.sh --issue --dns dns_cf --ocsp-must-staple --server letsencrypt  --keylength ec-384 -d %s --dnssleep 120 --days 90 --home '%s'" %(domain,domain)
+    #result = subprocess.run(command,shell=True)
+    #if (result.returncode == 0 ):
+        #print("Certificate renewed successfully.")
+        #cert_path = str("./"+domain + "_ecc/" + domain + ".cer")
+        result2 = subprocess.run(["scp -P 22 ","acme.sh","zeyad@office.3es-eg.uk:/home/zeyad/Desktop/" ])
         
 #------------the output ----------------
         if (result2.returncode == 0 ):
