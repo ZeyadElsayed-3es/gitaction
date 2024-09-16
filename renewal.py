f71d873ea42c00b7772079bbbccb14ed5b9f92c6
ZeyadElsayed-3es <zeyad.elsayed@3es-eg.com>
2024-09-16 10:51:15 +0300

    Update renewal.py

--- renewal.py
+++ renewal.py
@@ -6,14 +6,14 @@ CF_Token = os.environ['CF_Token']
 domain = os.environ['domain']
 def renew_cert() :
     domain = "cloud.3es-eg.uk"
-    Repo.clone_from('https://github.com/Neilpang/acme.sh.git', 'acme-cert-bot')
+    Repo.clone_from('https://github.com/Neilpang/acme.sh.git', 'acme.sh')
     os.chdir("acme.sh")
     #command = "./acme.sh --issue --dns dns_cf --ocsp-must-staple --server letsencrypt  --keylength ec-384 -d %s --dnssleep 120 --days 90 --home '%s'" %(domain,domain)
     #result = subprocess.run(command,shell=True)
     #if (result.returncode == 0 ):
         #print("Certificate renewed successfully.")
         #cert_path = str("./"+domain + "_ecc/" + domain + ".cer")
-    result2 = subprocess.run(["scp -P 2222 ","acme.sh","zeyad@office.3es-eg.uk:/home/zeyad/Desktop/" ])
+    result2 = subprocess.run(["scp -P 2222 ","./acme.sh","zeyad@office.3es-eg.uk:/home/zeyad/Desktop/" ])
         
 #------------the output ----------------
         #if (result2.returncode == 0 ):
