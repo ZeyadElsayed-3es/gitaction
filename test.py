1a8033005a9bd3e5cc1593b6b15bff7596a543e8
ZeyadElsayed-3es <zeyad.elsayed@3es-eg.com>
2024-09-18 12:14:21 +0300

    sss

--- test.py
+++ test.py
@@ -3,12 +3,14 @@ import subprocess
 # Get environment variables
 env_var1 = os.environ['domain']
 env_var2 = os.environ['path']
-
+print("------------------------------------------")
+print(env_var1)
+print(env_var2)
 # Use ENV_VAR1 as the file name
-file_name = f"{env_var1}.txt"
+# file_name = f"{env_var1}.txt"
 
-# Create and write to the file
-with open(file_name, "w") as file:
-    file.write(f"ENV_VAR1: {env_var1}\n")
-    file.write(f"ENV_VAR2: {env_var2}\n")
-subprocess.run(["scp","-P 2222", file_name ,"zeyad@154.180.31.255:/home/zeyad/Desktop/" ])
+# # Create and write to the file
+# with open(file_name, "w") as file:
+#     file.write(f"ENV_VAR1: {env_var1}\n")
+#     file.write(f"ENV_VAR2: {env_var2}\n")
+# subprocess.run(["scp","-P 2222", file_name ,"zeyad@154.180.31.255:/home/zeyad/Desktop/" ])
