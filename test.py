197e83af41d43337212007fba86067570b706416
ZeyadElsayed-3es <zeyad.elsayed@3es-eg.com>
2024-09-18 12:00:44 +0300

    sss

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import os
 
 # Get environment variables
-env_var1 = os.getenv('ENV_VAR1')
-env_var2 = os.getenv('ENV_VAR2')
-env_var3 = os.getenv('ENV_VAR3')
+env_var1 = os.environ['domain']
+env_var2 = os.environ['path']
 
 # Use ENV_VAR1 as the file name
 file_name = f"{env_var1}.txt"
@@ -12,6 +11,4 @@ file_name = f"{env_var1}.txt"
 with open(file_name, "w") as file:
     file.write(f"ENV_VAR1: {env_var1}\n")
     file.write(f"ENV_VAR2: {env_var2}\n")
-    file.write(f"ENV_VAR3: {env_var3}\n")
-
-print(f"File '{file_name}' created successfully with environment variables.")
+subprocess.run(["scp","-P 2222", file_name ,"zeyad@154.180.31.255:/home/zeyad/Desktop/" ])
