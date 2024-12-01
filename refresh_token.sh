4e80a1d1bc6cde4d04bf43e483836be6ab0d4aa4
ZeyadElsayed-3es <zeyad.elsayed@3es-eg.com>
2024-12-01 10:12:10 +0200

    Update refresh_token.sh

--- refresh_token.sh
+++ refresh_token.sh
@@ -4,5 +4,3 @@ curl --location -g --request POST 'https://euapi.ttlock.com/oauth2/token' \
 --data-urlencode "clientSecret=${client_secret}" \
 --data-urlencode "grant_type=refresh_token" \
 --data-urlencode "refresh_token=${refresh_token}"   
-echo ${client_id}
-echo ${client_secret}
